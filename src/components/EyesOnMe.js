ba6f01a1d9f08ad6d5816bd7cd7ebef9365088c4
boyraf <raphael.kymotho@gmail.com>
2023-04-24 09:31:27 +0300

    completed assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,22 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ 
+function EyesOnMe(){
+    function handleBlur(event){
+        event.preventDefault()
+        console.log('Hey! Eyes on me!')
+    }
+    
+    function handleFocus(event){
+        event.preventDefault()
+        console.log('Good!')
+    }
+
+    return (
+        <button onFocus={handleFocus} onBlur={handleBlur}>Eyes on me</button>
+    )
+ }
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,18 @@
 // Code Keypad Component Here
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 function Keypad (){
+    function handleChange(event){
+        event.preventDefault()
+        console.log('Entering password...')
+    }
     return (
-        <div></div>
+        <div>
+            <form>
+                <input type="password" onChange={handleChange}/>
+            </form>
+        </div>
     )
 }
 
